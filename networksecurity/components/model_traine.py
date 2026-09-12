a41eafacbcafffe4a7a97ceafa21911ea008c73b
Ramakant Kushwaha <ramakantkushwaha2006@gmail.com>
2026-09-12 16:57:44 +0530

    pipeline is completed

--- networksecurity/components/model_traine.py
+++ networksecurity/components/model_traine.py
@@ -24,7 +24,8 @@ from sklearn.ensemble import (
     RandomForestClassifier,
 )
 import mlflow
-from urllib.parse import urlparse
+import dagshub
+dagshub.init(repo_owner='ramakantkushwaha2006-beep', repo_name='networksecurity', mlflow=True)
 
 
 
@@ -132,7 +133,7 @@ class ModelTrainer:
         Network_Model=NetworkModel(preprocessor=preprocessor,model=best_model)
         save_object(self.model_trainer_config.trained_model_file_path,obj=NetworkModel)
         #model pusher
-        #save_object("final_model/model.pkl",best_model)
+        save_object("final_model/model.pkl",best_model)
         
 
         ## Model Trainer Artifact
